70b609b907af9eae766ec50bc9fec21a3188dec5
wahyuIES <44322104+wahyuIES@users.noreply.github.com>
2019-01-08 23:54:32 +0700

    Add files via upload

---
+++ IES_DIR.sh
@@ -0,0 +1,23 @@
+#Hi! HARGAI PEMBUAT NYA?
+#RECODE BUDIDAYAKAN IJIN  ^^
+#CONTACK : m.facebook.com/INDONESIAN ERROR SYSTE ^^
+
+figlet -f slant IES| lolcat
+
+echo  "_____________________________________________________________" | 
+lolcat
+echo  "[TYPE      : TOOLS SEARCH DIR WEBSITE] " |lolcat
+echo  "[AUTHOR    : MR.W4HYU ] " |lolcat
+echo  "[TEAM : INDONESIAN ERROR SYSTEM] " |lolcat
+echo  "[NOTES     : mastah just ilution] " | lolcat
+echo  "[THANKS TO : all member indonesian error system]" | lolcat
+echo  "_____________________________________________________________" | 
+lolcat
+
+sleep 1
+
+pkg install curl
+echo  "${y} {1} MASUKAN WEBSITE TARGET [Use HTTPS/HTTP]${endc}:" | lolcat
+read web
+curl https://api.hackertarget.com/pagelinks/?q=$web
+echo
